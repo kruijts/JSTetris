baa65003f203450657f6a5615fca87aaa6895f07
Sophie Kruijt <sophiekruijt@gmail.com>
2022-08-29 02:01:17 +0000

    devServer directory changed

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'assets')
+      directory: path.join(__dirname, 'dist')
     },
     compress: true,
     hot: true,
